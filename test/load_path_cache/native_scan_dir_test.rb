46bf2c9f982c569699651803ae1cbbccb6e332a2
Todd Fisher <todd.fisher@gmail.com>
2026-01-02 11:22:19 -0500

    Fix CI: skip native test without extension and stop Coverage

--- test/load_path_cache/native_scan_dir_test.rb
+++ test/load_path_cache/native_scan_dir_test.rb
@@ -9,12 +9,17 @@ module Bootsnap
       include LoadPathCacheHelper
 
       def test_sys_fail_with_zero_errno
-        unless Native.respond_to?(:__test_sys_fail_zero_errno)
+        unless Bootsnap::LoadPathCache.const_defined?(:Native)
+          skip("Native extension not available")
+        end
+
+        native = Bootsnap::LoadPathCache.const_get(:Native)
+        unless native.respond_to?(:__test_sys_fail_zero_errno)
           skip("Native test helper not available")
         end
 
         error = assert_raises(SystemCallError) do
-          Native.__test_sys_fail_zero_errno
+          native.__test_sys_fail_zero_errno
         end
 
         assert_kind_of(Errno::EIO, error)

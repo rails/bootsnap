c4b3d763b0cb93e0260a9e372ea9061e3b49ec81
Jason Karns <jason.karns@gmail.com>
2026-01-17 15:29:56 -0500

    Test rake entrypoint without triggering setup

--- test/rake_test.rb
+++ test/rake_test.rb
@@ -4,11 +4,18 @@ require "test_helper"
 
 module Bootsnap
   class RakeTest < Minitest::Test
+    def setup
+      @_old_cache_dir = Bootsnap.cache_dir
+      Bootsnap.instance_variable_set(:@cache_dir, Dir.mktmpdir("bootsnap-test"))
+    end
+
+    def teardown
+      Bootsnap.instance_variable_set(:@cache_dir, @_old_cache_dir)
+    end
+
     def test_cache_dir_is_cleaned
-      Bootsnap.setup(cache_dir: Dir.mktmpdir("bootsnap-test"))
       require "bootsnap/rake"
 
-      refute_nil(Bootsnap.cache_dir)
       assert_includes(CLEAN, Bootsnap.cache_dir)
     end
   end

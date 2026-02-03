93c35b83fd326ebfea4fb45f8cdd37c7c233222a
Jean Boussier <jean.boussier@gmail.com>
2026-02-03 09:10:41 +0100

    LoadPathScanner: Avoid computing the absolute path when not needed

--- lib/bootsnap/load_path_cache/path_scanner.rb
+++ lib/bootsnap/load_path_cache/path_scanner.rb
@@ -42,25 +42,6 @@ module Bootsnap
           requirables
         end
 
-        def walk(absolute_dir_path, relative_dir_path, &block)
-          Dir.foreach(absolute_dir_path) do |name|
-            next if name.start_with?(".")
-
-            relative_path = relative_dir_path ? File.join(relative_dir_path, name) : name
-
-            absolute_path = "#{absolute_dir_path}/#{name}"
-            if File.directory?(absolute_path)
-              next if ignored_directories.include?(name) || ignored_directories.include?(absolute_path)
-
-              if yield relative_path, absolute_path, true
-                walk(absolute_path, relative_path, &block)
-              end
-            else
-              yield relative_path, absolute_path, false
-            end
-          end
-        end
-
         if RUBY_ENGINE == "ruby" && RUBY_PLATFORM.match?(/darwin|linux|bsd|mswin|mingw|cygwin/)
           require "bootsnap/bootsnap"
         end
@@ -79,21 +60,28 @@ module Bootsnap
             # and the bundle path is '.bundle'.
             contains_bundle_path = BUNDLE_PATH.start_with?(root_path)
 
+            ignored_abs_paths, ignored_dir_names = ignored_directories.partition { |p| File.absolute_path?(p) }
+            ignored_abs_paths = nil if ignored_abs_paths.empty?
+            ignored_dir_names = nil if ignored_dir_names.empty?
+
             all_requirables, queue = Native.scan_dir(root_path)
             all_requirables.each(&:freeze)
 
             queue.reject! do |dir|
-              absolute_dir = File.join(root_path, dir)
-              ignored_directories.include?(dir) || ignored_directories.include?(absolute_dir) ||
-                (contains_bundle_path && absolute_dir.start_with?(BUNDLE_PATH))
+              if ignored_dir_names&.include?(dir)
+                true
+              elsif ignored_abs_paths || contains_bundle_path
+                absolute_dir = File.join(root_path, dir)
+                ignored_abs_paths&.include?(absolute_dir) ||
+                  (contains_bundle_path && absolute_dir.start_with?(BUNDLE_PATH))
+              end
             end
 
             while (path = queue.pop)
               absolute_base = File.join(root_path, path)
               requirables, dirs = Native.scan_dir(absolute_base)
               dirs.reject! do |dir|
-                absolute_dir = File.join(absolute_base, dir)
-                ignored_directories.include?(dir) || ignored_directories.include?(absolute_dir)
+                ignored_dir_names&.include?(dir) || ignored_abs_paths&.include?(File.join(absolute_base, dir))
               end
               dirs.map! { |f| File.join(path, f).freeze }
               requirables.map! { |f| File.join(path, f).freeze }
@@ -111,6 +99,31 @@ module Bootsnap
           alias_method :call, :native_call
         else
           alias_method :call, :ruby_call
+        end
+
+        private
+
+        def walk(absolute_dir_path, relative_dir_path, &block)
+          ignored_abs_paths, ignored_dir_names = ignored_directories.partition { |p| File.absolute_path?(p) }
+          ignored_abs_paths = nil if ignored_abs_paths.empty?
+          ignored_dir_names = nil if ignored_dir_names.empty?
+
+          Dir.foreach(absolute_dir_path) do |name|
+            next if name.start_with?(".")
+
+            relative_path = relative_dir_path ? File.join(relative_dir_path, name) : name
+
+            absolute_path = "#{absolute_dir_path}/#{name}"
+            if File.directory?(absolute_path)
+              next if ignored_dir_names&.include?(name) || ignored_abs_paths&.include?(absolute_path)
+
+              if yield relative_path, absolute_path, true
+                walk(absolute_path, relative_path, &block)
+              end
+            else
+              yield relative_path, absolute_path, false
+            end
+          end
         end
       end
     end
